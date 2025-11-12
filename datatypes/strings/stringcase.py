6b7f7e435a2114e32e1a90dac68eca4fcb79e5e0
sainaht1606 <sainathreddylingala94@gmail.com>
2025-11-12 22:47:31 +0530

    string excersices

--- datatypes/strings/stringcase.py
+++ datatypes/strings/stringcase.py
@@ -1,2 +1,19 @@
 name = "saianth reddy"
 print(name.upper())
+
+name2 = "SUJITHA REDDY"
+print(name2.lower())
+
+name3 = " kethana, Srivastavi, lingala"
+print(name3.title())
+print(name3.capitalize())
+print(name3.find('n'))
+print(name3.rfind('a'))
+print(name3.index('t'))
+print(name3.replace('kethana','lingala kethana'))
+print(name3.startswith('kethana'))
+print(name3.endswith('Srivastavi'))
+print(name3.split())
+print(name3.rsplit(',',1))
+print(name3.strip())
+
