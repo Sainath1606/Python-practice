10e9f44318729e6eac1237a48e805f54bd75f5cb
sainaht1606 <sainathreddylingala94@gmail.com>
2025-11-13 16:42:38 +0530

    adding numeric datatype

---
+++ datatypes/numbers/numeric.py
@@ -0,0 +1,13 @@
+x=1
+y=2.6123
+result1 = x + y
+result2 = x - y
+result3 = x * y
+result  = x / y
+print(result1)
+print(result3)
+print(result2)
+print(result)
+print(float(x))
+print(int(y))
+print(round(y , 1))
